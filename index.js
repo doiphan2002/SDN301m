787b7e93726203c302005fd651552ed6f4a36851
doiphan2002 <phanvangioi1361@gmail.com>
2024-09-22 00:58:56 +0700

    Mongoose Part 3

--- index.js
+++ index.js
@@ -1,33 +1,62 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
- const Nations = require('./models/nations')
+const Nations = require("./models/nations");
 
- const url = "mongodb://127.0.0.1:27017/football";
- const connect = mongoose.connect(url);
+const url = "mongodb://127.0.0.1:27017/football";
+const connect = mongoose.connect(url);
 
- connect.then((db) => {
+connect.then((db) => {
+  console.log("Connected correctly to server");
+  var newNation = Nations({
+    name: "Qatar",
+  });
+  newNation
+    .save()
+    .then((nation) => {
+      console.log(nation);
+      return Nations.find({}).exec();
+    })
+    .then((nations) => {
+      console.log(nations);
+      return Nations.remove({});
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
- console.log('Connected correctly to server');
-
- var newNation = Nations({
- name: 'Qatar',
- });
-
- newNation.save()
- .then((nation) => {
- console.log(nation);
- return Nations.find({}).exec();
- })
- .then((nations) => {
- console.log(nations);
-
- return Nations.remove({});
- })
- .then(() => {
- return mongoose.connection.close();
- })
- .catch((err) => {
- console.log(err);
- });
-
- });
+  Nations.create({
+    name: "Vietnam",
+    description: "Home Team",
+  })
+    .then((nation) => {
+      console.log(nation);
+      return Nations.findByIdAndUpdate(
+        nation._id,
+        {
+          $set: { description: "WC 2022" },
+        },
+        {
+          new: true,
+        }
+      ).exec();
+    })
+    .then((nation) => {
+      console.log(nation);
+      nation.commnets.push({
+        rating: 5,
+        commnet: "Please give me beer!",
+        author: "Hacker",
+      });
+      return nation.save();
+    })
+    .then((nation) => {
+      console.log(nation);
+      return Nations.deleteOne({});
+    })
+    .then(() => {
+      return mongoose.connection.close();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
--- models/nations.js
+++ models/nations.js
@@ -1,15 +1,45 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const nationSchema = new Schema({
+const commnetSchema = new Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    commnet: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const nationSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        unique: true
-    }
-}, {
-    timestamps: true
-})
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    commnets:[commnetSchema]
+  },
+  {
+    timestamps: true,
+  }
+);
 
-var Nations = mongoose.model('nations', nationSchema)
-module.exports = Nations
+var Nations = mongoose.model("nations", nationSchema);
+module.exports = Nations;
